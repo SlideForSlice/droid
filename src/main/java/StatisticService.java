4fb7755da5dd667dc81d8dd336a83128d327f56e
SlideForSlice <kenigilya9@gmail.com>
2023-06-28 08:30:18 +0500

    Added StatisticService

---
+++ src/main/java/StatisticService.java
@@ -0,0 +1,20 @@
+public class StatisticService {
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - assay of incomes
+     * @return - index of first max value
+     */
+
+    public long findMax(long[] incomes){
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+
+            return current_max;
+        }
+
+
+}
